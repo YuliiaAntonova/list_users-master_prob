4d2e8bd2847dfcf0ea5823db8c52bd2cfb9deaea
Yuliia Antonova <yuliiaantonova@Yuliias-MacBook-Pro.local>
2022-07-27 17:40:53 +0200

    test

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def create_fake_users(n):
         db.session.add(user)
     db.session.commit()
     print(f'Added {n} fake users to the database.')
-create_fake_users(10)
+
 
 
 
@@ -112,5 +112,5 @@ if __name__ == '__main__':
     # if len(sys.argv) <= 1:
     #     print('Pass the number of users you want to create as an argument.')
     #     sys.exit(1)
-
+    create_fake_users(10)
     app.run()
